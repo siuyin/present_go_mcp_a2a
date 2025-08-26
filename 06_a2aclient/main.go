bc1825eaacf85ba7ea966bd5cfc68ea3a97b02e1
Loh Siu Yin <siuyin@beyondbroadcast.com>
2025-08-26 09:55:09 +0800

    doc: update slides

--- 06_a2aclient/main.go
+++ 06_a2aclient/main.go
@@ -19,6 +19,7 @@ import (
 func main() {
 	prompt := dflt.EnvString("PROMPT", "Do you have the iPhone 14?")
 	res := inventoryLookup(prompt)
+
 	formatResponse(fmt.Sprintf(`user query: %s
 	response from inventory lookup: %s
 	Please answer the user query in a concise and professional manner`, prompt, res))
